965a345a207965a7c162db497d5b71448dc1eeef
OleksandrKyrylenko <kirilenko9910@ukr.net>
2022-07-31 19:17:58 +0300

    _

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const 	gulp = require('gulp'),
+		rename = require('gulp-rename'),
+		del = require('del'),
+		concat = require('gulp-concat'),
+		//imagemin = require('gulp-imagemin'),
+		uglify = require('gulp-uglify-es').default,
+		server = require('browser-sync').create();	
+	
+const clean = function() {
+	return del('dist');
+};
+
+const pug2html = function() {
+	return gulp.src('app/**/*.html')
+	.pipe(gulp.dest('./dist/'));
+};
+
+const styles = function() {
+	return gulp.src('app/css/*.css')
+	.pipe(gulp.dest('dist/css'));
+};
+
+const scripts = function() {
+	return gulp.src('app/js/*.js')
+	.pipe(concat('script.js'))
+	.pipe(uglify(''))
+	.pipe(gulp.dest('dist/js'));
+};
+
+const compress = function() {
+    return gulp.src('app/img/**')
+    .pipe(gulp.dest('dist/img/'))
+};
+
+const serverFunc = function() {
+	server.init({
+		server: "dist",
+		notify: false,
+		files: ['./dist/**/*.html','./dist/js/*.js','./dist/css/*.css']
+	})
+	
+	gulp.watch('app/css/*.css', gulp.series(styles)).on('change', server.reload);
+	gulp.watch('app/js/*.js', gulp.series(scripts)).on('change', server.reload);	
+	gulp.watch('app/**/*.html', gulp.series(pug2html)).on('change', server.reload);
+	gulp.watch('build/*.html').on('change', server.reload);
+};
+
+const dev = gulp.parallel( pug2html, styles, scripts, compress)
+
+const build = gulp.series(clean, dev)
+
+module.exports.default = gulp.series(build, serverFunc)
